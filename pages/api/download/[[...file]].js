796a3fe9ff997e4b718921a9966f9d3ef1ed5b97
Kanersps <kanepetra@gmail.com>
2021-11-26 17:52:00 +0100

    tweak: remove AWS import

--- pages/api/download/[[...file]].js
+++ pages/api/download/[[...file]].js
@@ -1,5 +1,3 @@
-import AWS from "@aws-sdk";
-
 import { GetObjectCommand, ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { promisify } from "util";
