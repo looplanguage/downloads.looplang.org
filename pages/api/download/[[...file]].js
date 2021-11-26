391b0201de2bd4f314e468a6929924d68ef28aa2
Kanersps <kanepetra@gmail.com>
2021-11-26 16:59:33 +0100

    feat: use s3 for files

---
+++ pages/api/download/[[...file]].js
@@ -0,0 +1,28 @@
+import AWS from "@aws-sdk";
+
+import { GetObjectCommand, ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { promisify } from "util";
+import stream from "stream";
+
+export default async function handler(req, res) {
+  const { file } = req.query;
+
+  const location = file.join("/");
+
+  const s3 = new S3Client({ region: "us-east-2" });
+  const pipeline = promisify(stream.pipeline);
+
+  let command = new GetObjectCommand({
+    Bucket: "loopartifacts",
+    Key: location,
+  });
+
+  const signedUrl = await getSignedUrl(s3, command, {
+    expiresIn: 3600,
+  });
+
+  const response = await fetch(signedUrl);
+
+  await pipeline(response.body, res);
+}
--- pages/api/prereleases.js
+++ pages/api/prereleases.js
@@ -1,16 +1,47 @@
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import * as AWS from "@aws-sdk/client-s3";
 
-export default function handler(req, res) {
-  const s3 = new S3Client({ region: "us-east-2" });
+async function getAllObjects(continuationKey, response) {
+  const client = new AWS.S3({ region: "us-east-2", credentials: { accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET } });
 
-  s3.send(
-    new ListObjectsV2Command({
-      Bucket: "loopartifacts",
-      Prefix: "Prerelease/",
-    })
-  ).then((result) => {
-    console.log(result.Contents);
-
-    res.send(200);
+  const result = await client.listObjectsV2({
+    Bucket: "loopartifacts",
+    Prefix: "Prerelease/",
   });
+
+  result.Contents.map((object) => {
+    const key = object.Key;
+    const parts = key.split("_");
+
+    const build = parts[0].replace("Prerelease/", "");
+    const os = parts[1];
+    const architecture = parts[2];
+    const version = parts[4].split(".").slice(0, 3).join(".");
+    const extension = parts[4].split(".").slice(3, 4);
+    const filename = parts[3] + (extension && "." + extension);
+    const released = object.LastModified;
+
+    response.push({
+      key,
+      build,
+      os,
+      architecture,
+      version,
+      filename,
+      released,
+    });
+  });
+
+  if (result.IsTruncated) {
+    return getAllObjects(result.ContinuationToken, response);
+  }
+
+  return response;
+}
+
+export default async function handler(req, res) {
+  let response = await getAllObjects("", []);
+
+  response = response.sort((a, b) => b.key.localeCompare(a.key));
+
+  res.send(response);
 }
