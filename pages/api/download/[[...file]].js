e474b9929a88b85d0166a30f357866d41dea186d
Kanersps <kanepetra@gmail.com>
2021-11-26 18:07:02 +0100

    tweak: use proper apis

--- pages/api/download/[[...file]].js
+++ pages/api/download/[[...file]].js
@@ -1,26 +1,23 @@
-import { GetObjectCommand, ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import * as AWS from "@aws-sdk/client-s3";
 import { promisify } from "util";
 import stream from "stream";
+import fs from "fs";
 
 export default async function handler(req, res) {
+  const client = new AWS.S3({ region: "us-east-2", credentials: { accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET } });
+
   const { file } = req.query;
 
   const location = file.join("/");
 
-  const s3 = new S3Client({ region: "us-east-2" });
-  const pipeline = promisify(stream.pipeline);
-
-  let command = new GetObjectCommand({
+  const { Body } = await client.getObject({
     Bucket: "loopartifacts",
     Key: location,
   });
 
-  const signedUrl = await getSignedUrl(s3, command, {
-    expiresIn: 3600,
+  await new Promise((resolve, reject) => {
+    Body.pipe(res)
+      .on("error", (err) => reject(err))
+      .on("close", () => resolve());
   });
-
-  const response = await fetch(signedUrl);
-
-  await pipeline(response.body, res);
 }
